586bbed439c4aebd2dae7185c45efa51d78123b6
Ronald Tse <ronald.tse@ribose.com>
2024-05-14 16:20:46 +0800

    feat: initial commit

---
+++ lib/xml/c14n/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Xml
+  module C14n
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/xml/c14n_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe Xml::C14n do
+  it "has a version number" do
+    expect(Xml::C14n::VERSION).not_to be nil
+  end
+
+  Dir.glob("spec/fixtures/*.raw.xml").each do |f|
+    c14n_filename = f.gsub(".raw.", ".c14n.")
+    subject(:xml_raw) { File.open(f) }
+    let(:xml_c14n) { File.open(c14n_filename) }
+
+    it "canonicalizes #{File.basename(f)}" do
+      input = Xml::C14n.format(File.read(xml_raw))
+      output = Xml::C14n.format(File.read(xml_c14n))
+
+      expect(output).to eq(input)
+    end
+  end
+end
