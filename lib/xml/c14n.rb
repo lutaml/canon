6e55e29f069846ccc9990ff4f1a47951b3d32055
Ronald Tse <ronald.tse@ribose.com>
2024-05-14 16:41:12 +0800

    chore: clean up template and provide source

--- lib/xml/c14n.rb
+++ lib/xml/c14n.rb
@@ -5,11 +5,12 @@ require "nokogiri"
 
 module Xml
   module C14n
+    # Source of XSLT
+    # https://emmanueloga.wordpress.com/2009/09/29/pretty-printing-xhtml-with-nokogiri-and-xslt/
     NOKOGIRI_C14N_XSL = <<~XSL
       <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
         <xsl:output method="xml" encoding="ISO-8859-1"/>
         <xsl:param name="indent-increment" select="'   '"/>
-
         <xsl:template name="newline">
           <xsl:text disable-output-escaping="yes">
       </xsl:text>
@@ -35,21 +36,21 @@ module Xml
           <xsl:param name="indent" select="''"/>
           <xsl:call-template name="newline"/>
           <xsl:value-of select="$indent"/>
-            <xsl:choose>
-             <xsl:when test="count(child::*) > 0">
+          <xsl:choose>
+            <xsl:when test="count(child::*) > 0">
               <xsl:copy>
-               <xsl:copy-of select="@*"/>
-               <xsl:apply-templates select="*|text()">
-                 <xsl:with-param name="indent" select="concat ($indent, $indent-increment)"/>
-               </xsl:apply-templates>
-               <xsl:call-template name="newline"/>
-               <xsl:value-of select="$indent"/>
+              <xsl:copy-of select="@*"/>
+              <xsl:apply-templates select="*|text()">
+                <xsl:with-param name="indent" select="concat ($indent, $indent-increment)"/>
+              </xsl:apply-templates>
+              <xsl:call-template name="newline"/>
+              <xsl:value-of select="$indent"/>
               </xsl:copy>
-             </xsl:when>
-             <xsl:otherwise>
+            </xsl:when>
+            <xsl:otherwise>
               <xsl:copy-of select="."/>
-             </xsl:otherwise>
-           </xsl:choose>
+            </xsl:otherwise>
+          </xsl:choose>
         </xsl:template>
       </xsl:stylesheet>
     XSL
